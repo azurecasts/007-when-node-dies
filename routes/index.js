4d94f5c4b6c345a8ce1738e083ba63dca52a990e
Burke Holland <burkeholland@gmail.com>
2019-05-04 11:28:07 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var router = express.Router();
+var fs = require("fs");
+
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/read", function(req, res, next) {
+  try {
+    const book = fs.createReadStream("the-illiad.pdf");
+    book.pipe(res);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
